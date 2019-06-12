ede4e520da2f49e459e69ba19b90c47fc80d5329
Paulo Henrique da Silva Fernandes <pfernandes@braspag.com.br>
2019-06-12 09:22:06 -0300

    Initial project.

---
+++ part1/Pfernandes.MathBasic.Core/Pfernandes.MathBasic.App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Pfernandes.MathBasic.Core;
+
+namespace Pfernandes.MathBasic.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quadraticEquation = new QuadraticEquation();
+
+            Console.WriteLine("Digite A");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite B");
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite C");
+            var c = double.Parse(Console.ReadLine());
+
+            var x = quadraticEquation.Solve(a, b, c);
+            
+            Console.WriteLine($"X1: {x.x1:N}");
+            Console.WriteLine($"X2: {x.x2:N}");
+        }
+    }
+}
---
+++ part1/Pfernandes.MathBasic.Core/Pfernandes.MathBasic.Core/QuadraticEquation.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Pfernandes.MathBasic.Core
+{
+    public class QuadraticEquation
+    {
+        public (double x1, double x2) Solve(double a, double b, double c)
+        {
+            if(a == 0)
+                throw new ArgumentException("a must not be zero", nameof(a));
+
+            var insideRoot = (b * b) - 4 * a * c;
+            var root = Math.Sqrt(insideRoot);
+
+            var x1 = (-b + root) / (2 * a);
+            var x2 = (-b - root) / (2 * a);
+
+            return (x1, x2);
+        }
+    }
+}
